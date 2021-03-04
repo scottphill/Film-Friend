b2bcfd8fffcae83d2c32cd2ac46f90a51042c61f
Scott Hill <scottphill98@gmail.com>
2021-03-04 15:03:47 -0500

    Added respository and TMDB api functions. No threading yet so they aren't functional to the UI yet. Also added gradle dependencies for Retrofit.

--- Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/MovieRepository.java
+++ Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/MovieRepository.java
@@ -2,8 +2,14 @@ package com.usf_mobile_dev.filmfriend;
 
 import android.app.Application;
 
+import com.usf_mobile_dev.filmfriend.api.TMDBApi;
+
 public class MovieRepository {
     public MovieRepository(Application application){
 
+    }
+
+    public void getMovie(){
+        TMDBApi.getMovie();
     }
 }
