670c0b8d2dd5071ac62f4534f697cdd780372da3
GoodPalAl <63667198+GoodPalAl@users.noreply.github.com>
2021-03-09 18:38:51 -0500

    Created QR camera functionality
    Camera opens when "scan QR" button is pressed. Asks user for camera permissions. Displays an error when user denied, continues when accepted. Currently only displays decoded message into a TextView.

---
+++ Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/ui/qr/QRCameraActivity.java
@@ -0,0 +1,123 @@
+package com.usf_mobile_dev.filmfriend.ui.qr;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.budiyev.android.codescanner.AutoFocusMode;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ErrorCallback;
+import com.budiyev.android.codescanner.ScanMode;
+import com.google.zxing.Result;
+
+import com.usf_mobile_dev.filmfriend.R;
+
+//https://github.com/yuriy-budiyev/code-scanner
+public class QRCameraActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST_CODE = 101;
+    private CodeScanner mCodeScanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        setUpPermissions();
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setCamera(CodeScanner.CAMERA_BACK);
+        mCodeScanner.setFormats(CodeScanner.ALL_FORMATS);
+        mCodeScanner.setAutoFocusMode(AutoFocusMode.SAFE);
+        mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
+        mCodeScanner.setAutoFocusEnabled(true);
+        mCodeScanner.setFlashEnabled(false);
+
+        //When camera finds a qr code to scan
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Toast.makeText(QRCameraActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        TextView text_view = findViewById(R.id.tv_textview);
+                        text_view.setText(result.getText());
+                    }});
+            }});
+
+        //Error callback
+        mCodeScanner.setErrorCallback(new ErrorCallback() {
+            @Override
+            public void onError(@NonNull Exception error) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e("QRCamera", "Camera initialization error: " + error.getMessage());
+                    }
+                });
+            }
+        });
+
+        //Continuously looks for a new code
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }});
+    }
+
+    //Lifecycle methods
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+    private void setUpPermissions() {
+        int permission = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA);
+
+        if (permission != PackageManager.PERMISSION_GRANTED)
+        {
+            makePermissionRequest();
+        }
+    }
+    private void makePermissionRequest()
+    {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Enable camera permission to use this section of the app.", Toast.LENGTH_LONG).show();
+            } else {
+                //successful request: do nothing!
+            }
+        }
+    }
+}
