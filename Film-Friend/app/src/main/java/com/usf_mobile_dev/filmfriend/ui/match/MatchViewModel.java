c7462036de775039fea4f914b0f1ce6b2957bd2b
Scott Hill <scottphill98@gmail.com>
2021-03-16 12:55:37 -0400

    Added a bunch of match features, including languages, genres you don't want, and matching without repeat movies.

--- Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/ui/match/MatchViewModel.java
+++ Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/ui/match/MatchViewModel.java
@@ -3,23 +3,26 @@ package com.usf_mobile_dev.filmfriend.ui.match;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.widget.Toast;
-import androidx.core.util.Pair;
-import androidx.fragment.app.FragmentActivity;
+
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.usf_mobile_dev.filmfriend.Movie;
 import com.usf_mobile_dev.filmfriend.MovieRepository;
-import com.usf_mobile_dev.filmfriend.RepositoryCallback;
-import com.usf_mobile_dev.filmfriend.ThreadResult;
+import com.usf_mobile_dev.filmfriend.R;
 import com.usf_mobile_dev.filmfriend.api.DiscoverResponse;
+import com.usf_mobile_dev.filmfriend.api.GenreResponse;
+import com.usf_mobile_dev.filmfriend.api.LanguageResponse;
 import com.usf_mobile_dev.filmfriend.ui.movieInfo.MovieInfoActivity;
+import com.usf_mobile_dev.filmfriend.ui.movieInfo.MovieInfoViewModel;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,8 +31,14 @@ import retrofit2.Response;
 public class MatchViewModel extends AndroidViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<List<GenreResponse.Genre>> genres;
+    private HashMap<String, Integer> genres_to_api_id;
+    private MutableLiveData<List<LanguageResponse>> languages;
+    private HashMap<String, String> languages_to_iso_id;
+    private MutableLiveData<String> selectedLanguage;
     private MovieRepository movieRepository;
     private MatchPreferences MP;
+    private String api_key;
 
     public MatchViewModel(Application application) {
         super(application);
@@ -37,8 +46,20 @@ public class MatchViewModel extends AndroidViewModel {
         mText.setValue("Filters:");
         movieRepository = new MovieRepository(application);
         MP = new MatchPreferences();
+        api_key = application.getString(R.string.tmdb_api_key);
+        genres = new MutableLiveData<List<GenreResponse.Genre>>();
+        genres.setValue(new ArrayList<GenreResponse.Genre>());
+        genres_to_api_id = new HashMap<String, Integer>();
+        this.refreshGenres(application);
+        languages = new MutableLiveData<>();
+        languages.setValue(new ArrayList<>());
+        languages_to_iso_id = new HashMap<>();
+        selectedLanguage = new MutableLiveData<>();
+        selectedLanguage.postValue("");
+        this.refreshLanguages(application);
     }
 
+    /*
     // https://api.themoviedb.org/3/genre/movie/list?api_key=25ace50f784640868b88295ea133e67e&language=en-US
     final private HashMap<String, Integer> genres_to_api_id = new HashMap<String, Integer>()
     {{
@@ -62,6 +83,7 @@ public class MatchViewModel extends AndroidViewModel {
         put("War", 10752);
         put("Western", 37);
     }};
+     */
     final private HashMap<String, Integer> watch_providers_to_api_id = new HashMap<String, Integer>()
     {{
         put("Netflix", 8);
@@ -71,10 +93,16 @@ public class MatchViewModel extends AndroidViewModel {
         put("Google Play", 3);
     }};
 
-    public void setGenreVal (String name, Boolean new_val)
+    public void setIncludedGenreVal(String name, Boolean new_val)
     {
         Integer id = genres_to_api_id.get(name);
-        MP.setGenre(id, new_val);
+        MP.setIncludedGenre(id, new_val);
+    }
+
+    public void setExcludedGenreVal(String name, Boolean new_val)
+    {
+        Integer id = genres_to_api_id.get(name);
+        MP.setExcludedGenre(id, new_val);
     }
 
     public void setWPVal (String name, Boolean new_val)
@@ -83,7 +111,7 @@ public class MatchViewModel extends AndroidViewModel {
         MP.setWatchProvider(id, new_val);
     }
 
-    public void setRating (int new_rating, boolean is_min)
+    public void setRating (double new_rating, boolean is_min)
     {
         if (is_min)
             MP.setRating_min(new_rating);
@@ -151,8 +179,16 @@ public class MatchViewModel extends AndroidViewModel {
                                     MovieInfoActivity.class
                             );
                             movieActivityIntent.putExtra(
-                                    MovieInfoActivity.INTENT_EXTRAS_MOVIE_DATA,
+                                    MovieInfoViewModel.INTENT_EXTRAS_MOVIE_DATA,
                                     movie);
+                            movieActivityIntent.putExtra(
+                                    MovieInfoViewModel.INTENT_EXTRAS_ACTIVITY_MODE,
+                                    MovieInfoViewModel.ACTIVITY_MODE_MATCH
+                            );
+                            movieActivityIntent.putExtra(
+                                    MovieInfoViewModel.INTENT_EXTRAS_MOVIE_PREFERENCES,
+                                    getMP()
+                            );
                             context.startActivity(movieActivityIntent);
                         }
                         // Pops a toast if there aren't any movies returned
@@ -168,21 +204,112 @@ public class MatchViewModel extends AndroidViewModel {
                     }
                 },
                 context.getMainExecutor(),
-                MP);
-            /*new RepositoryCallback<DiscoverResponse>() {
-                @Override
-                public void onComplete(ThreadResult<DiscoverResponse> result) {
-                    if (result instanceof ThreadResult.Success) {
-                        // Happy Path
+                MP,
+                this.api_key);
+    }
+
+    private void refreshGenres(Context context) {
+        movieRepository.getTMDBGenres(
+                new Callback<GenreResponse>() {
+                    @Override
+                    public void onResponse(Call<GenreResponse> call,
+                                           Response<GenreResponse> response) {
+                        GenreResponse results = response.body();
+                        if (results != null) {
+
+                            // Creates a new genres to id HashMap
+                            genres_to_api_id.clear();
+                            for(GenreResponse.Genre  genre : results.genres)
+                                genres_to_api_id.put(genre.name, genre.id);
+
+                            // Resets the genres in the current Match Preference
+                            MP.clearGenres();
+                            MP.setGenres(results.genres);
+                            genres.setValue(results.genres);
+                        }
                     }
-                    else {
-                        // Error Happened - Show to User
+
+                    @Override
+                    public void onFailure(Call<GenreResponse> call,
+                                          Throwable t) {
+                        call.cancel();
+                        Toast.makeText(context,
+                                "COULDN'T ACCESS GENRES",
+                                Toast.LENGTH_LONG).show();
                     }
-                }
-            });*/
+                },
+                context.getMainExecutor(),
+                this.api_key
+        );
+    }
+
+    private void refreshLanguages(Context context) {
+        movieRepository.getTMDBLanguages(
+                new Callback<List<LanguageResponse>>() {
+                    @Override
+                    public void onResponse(
+                            Call<List<LanguageResponse>> call,
+                            Response<List<LanguageResponse>> response
+                    ) {
+                        List<LanguageResponse> results = response.body();
+                        if(results != null) {
+
+                            // Creates a new languages list
+                            languages_to_iso_id.clear();
+                            for(LanguageResponse  language : results)
+                                languages_to_iso_id.put(
+                                        language.english_name,
+                                        language.iso_code);
+
+                            // Resets the genres in the current Match Preference
+                            results.sort(new Comparator<LanguageResponse>() {
+                                @Override
+                                public int compare(LanguageResponse o1, LanguageResponse o2) {
+                                    return o1.english_name.compareTo(o2.english_name);
+                                }
+                            });
+                            languages.postValue(results);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(
+                            Call<List<LanguageResponse>> call,
+                            Throwable t
+                    ) {
+                        call.cancel();
+                        Toast.makeText(context,
+                                "COULDN'T ACCESS LANGUAGES",
+                                Toast.LENGTH_LONG).show();
+                    }
+                },
+                context.getMainExecutor(),
+                this.api_key
+        );
+    }
+
+    public MutableLiveData<List<GenreResponse.Genre>> getGenres() {
+        return genres;
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public MatchPreferences getMP() {
+        return this.MP;
+    }
+
+    public MutableLiveData<List<LanguageResponse>> getLanguages() {
+        return this.languages;
+    }
+
+    public MutableLiveData<String> getSelectedLanguage() {
+        return this.selectedLanguage;
+    }
+
+    public void setSelectedLanguage(String language) {
+        this.selectedLanguage.postValue(language);
+        this.MP.setSelected_language(languages_to_iso_id.get(language));
     }
 }
--- Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/ui/movieInfo/MovieInfoActivity.java
+++ Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/ui/movieInfo/MovieInfoActivity.java
@@ -1,74 +1,95 @@
 package com.usf_mobile_dev.filmfriend.ui.movieInfo;
 
-import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.bumptech.glide.Glide;
 import com.usf_mobile_dev.filmfriend.Movie;
 import com.usf_mobile_dev.filmfriend.R;
-import java.net.URL;
+import com.usf_mobile_dev.filmfriend.ui.match.MatchPreferences;
 
 
 public class MovieInfoActivity extends AppCompatActivity {
-    public static String INTENT_ACTION_LAUNCH_WITH_MOVIE_DATA = "com.usf_mobile_dev.filmfriend.intent.action.launch_with_movie_data";
-    public static String INTENT_EXTRAS_MOVIE_DATA = "com.usf_mobile_dev.filmfriend.intent.extras.movie_data";
 
     private MovieInfoViewModel movieInfoViewModel;
-    private Movie movie;
-
     private TextView mMovieTitle;
     private TextView mMovieRelease;
     private TextView mRating;
     private TextView mVoteCount;
     private TextView mMovieOverview;
-
     private ImageView mMovieBanner;
     private ImageView mMoviePoster;
+    private Button newMovieBtn;
+    private Button watchMovieBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
 
-        //movieInfoViewModel = new ViewModelProvider(this).get(MovieInfoViewModel.class);
+        movieInfoViewModel = new ViewModelProvider(this).get(MovieInfoViewModel.class);
 
         mMovieTitle = findViewById(R.id.movie_info_title);
         mMovieRelease = findViewById(R.id.movie_info_release);
         mRating = findViewById(R.id.rating);
         mVoteCount = findViewById(R.id.vote_count);
         mMovieOverview = findViewById(R.id.movie_info_overview);
-
         mMovieBanner = findViewById(R.id.movie_banner);
         mMoviePoster = findViewById(R.id.movie_poster);
+        newMovieBtn = findViewById(R.id.button_new_movie);
+        watchMovieBtn = findViewById(R.id.button_watch_movie);
 
-        ///*
         if(getIntent().getExtras() != null) {
-            movie = (Movie) getIntent().getSerializableExtra(INTENT_EXTRAS_MOVIE_DATA);
-
-            mMovieTitle.setText(movie.getTitle());
-            mMovieRelease.setText(movie.getReleaseYearAsStr());
-            mRating.setText(movie.getRatingAsFormattedStr());
-            mVoteCount.setText(movie.getVoteCountAsString());
-            mMovieOverview.setText(movie.getOverview());
-
-            String posterUrl = getString(R.string.tmdb_image_base_url)
-                    + getString(R.string.tmdb_poster_size_3)
-                    + movie.getPosterPath();
-            String backdropUrl = getString(R.string.tmdb_image_base_url)
-                    + getString(R.string.tmdb_backdrop_size_2)
-                    + movie.getBackdropPath();
-
-            Glide.with(this)
-                    .load(posterUrl)
-                    .into(mMoviePoster);
-
-            Glide.with(this)
-                    .load(backdropUrl)
-                    .into(mMovieBanner);
-
+            Movie newMovie = (Movie)getIntent()
+                    .getSerializableExtra(MovieInfoViewModel.INTENT_EXTRAS_MOVIE_DATA);
+            setMovieDetails(newMovie);
+            movieInfoViewModel.setCurrentMovie(newMovie);
+            movieInfoViewModel.setActivityMode(getIntent()
+                    .getStringExtra(MovieInfoViewModel.INTENT_EXTRAS_ACTIVITY_MODE));
+            movieInfoViewModel.setCurMatchPreferences((MatchPreferences)getIntent()
+                    .getSerializableExtra(MovieInfoViewModel.INTENT_EXTRAS_MOVIE_PREFERENCES));
         }
-        //*/
+
+        movieInfoViewModel.getCurrentMovie().observe(
+                this,
+                currentMovie -> {
+                    if(currentMovie != null)
+                        setMovieDetails(currentMovie);
+                }
+        );
+
+        newMovieBtn.setOnClickListener(movieInfoViewModel
+                .getNewMovieBtnOnClickListener());
+        watchMovieBtn.setOnClickListener(movieInfoViewModel
+                .getWatchMovieOnClickListener());
+    }
+
+    // Sets the UI elements of this activity to show info for a new movie
+    public void setMovieDetails(Movie newMovie) {
+
+        mMovieTitle.setText(newMovie.getTitle());
+        mMovieRelease.setText(newMovie.getReleaseYearAsStr());
+        mRating.setText(newMovie.getRatingAsFormattedStr());
+        mVoteCount.setText(newMovie.getVoteCountAsString());
+        mMovieOverview.setText(newMovie.getOverview());
+
+        String posterUrl = getString(R.string.tmdb_image_base_url)
+                + getString(R.string.tmdb_poster_size_3)
+                + newMovie.getPosterPath();
+        String backdropUrl = getString(R.string.tmdb_image_base_url)
+                + getString(R.string.tmdb_backdrop_size_2)
+                + newMovie.getBackdropPath();
+
+        Glide.with(this)
+                .load(posterUrl)
+                .into(mMoviePoster);
+
+        Glide.with(this)
+                .load(backdropUrl)
+                .into(mMovieBanner);
     }
 }
