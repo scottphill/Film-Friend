1695c70b65f498363a5f54ff979405a352e089c0
GoodPalAl <63667198+GoodPalAl@users.noreply.github.com>
2021-03-27 17:15:14 -0400

    ham menu button appears but drawer does not pull out

--- Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/MainActivity.java
+++ Film-Friend/app/src/main/java/com/usf_mobile_dev/filmfriend/MainActivity.java
@@ -3,10 +3,12 @@ package com.usf_mobile_dev.filmfriend;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +28,19 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // FIXME: does not pull out
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -37,21 +52,38 @@ public class MainActivity extends AppCompatActivity
         NavigationUI.setupActionBarWithNavController(
                 this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
 
-        /*
-        // FIXME
+    /**
+     * Inflates the options menu.
+     * @param menu  Menu to inflate
+     * @return      Returns true if menu is inflated.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.ham_menu, menu);
+        return true;
+    }
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        if (drawer != null) {
-            drawer.addDrawerListener(toggle);
+    /**
+     * Handles a click on the Settings item in the options menu.
+     * @param item  Item in options menu that was clicked.
+     * @return      Returns true if the item was Settings.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.drawer_layout) {
+            return true;
         }
-        toggle.syncState();
-        //*/
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
